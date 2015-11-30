701b377f981a92a1773d93e54c17cd0410e6a5ad
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2015-11-30 15:52:53 +0200

    Use Clojure 1.7.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-difftest "2.0.0"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [criterium "0.4.3"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [org.clojure/math.combinatorics "0.0.7"]])
