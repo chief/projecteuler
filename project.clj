0a48ec782cdb3007ad7a78f2771043c9f4f9a5cd
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2015-11-30 16:01:46 +0200

    Use prismatic Schema

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [criterium "0.4.3"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/math.combinatorics "0.1.1"]])
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 [prismatic/schema "1.0.3"]])
